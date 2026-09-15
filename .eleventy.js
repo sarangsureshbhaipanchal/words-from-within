069f79959eb590fd1792e762ca5d62223e3dce15
sarangsureshbhaipanchal <sarang.sureshbhai.panchal@gmail.com>
2026-09-15 21:46:11 +0530

    Improve article date format

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,30 @@
-module.exports=function(eleventyConfig){eleventyConfig.addPassthroughCopy({"public":"."});eleventyConfig.addCollection("posts",api=>api.getFilteredByGlob("src/posts/*.md").sort((a,b)=>b.date-a.date));return{dir:{input:"src",includes:"_includes",output:"_site"}}};
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("readableDate", function(value) {
+    const date = new Date(value);
+
+    return new Intl.DateTimeFormat("en-IN", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      timeZone: "Asia/Kolkata"
+    }).format(date);
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    "public": ""
+  });
+
+  eleventyConfig.addCollection("posts", api =>
+    api.getFilteredByGlob("src/posts/*.md")
+      .sort((a, b) => b.date - a.date)
+  );
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
